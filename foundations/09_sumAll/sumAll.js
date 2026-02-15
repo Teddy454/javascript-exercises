6f8e6a87127d4e403be58d16052f4fc89b449a04
Tom Fleming <tomfleming8@gmail.com>
2026-02-15 16:46:06 +0000

    Exercise complete.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let result = 0;
 
+    if (int1 < 0 || int2 < 0) {
+        return `ERROR`
+    }   else if (!Number.isInteger(int1)  || !Number.isInteger(int2)) {
+        return `ERROR`
+    }   else {
+        for (let i = Math.min(int1, int2); i <= Math.max(int1, int2); i++) {
+            result += i;
+        }
+    }   
+
+    return result;
 };
 
 // Do not edit below this line

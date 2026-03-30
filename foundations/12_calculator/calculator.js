e32b9c4233ac61a952b4e1532c04b09bdff0b5d6
Tom Fleming <tomfleming8@gmail.com>
2026-03-30 21:01:11 +0100

    Exercise complete.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	const result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	const result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const result = array.reduce((total, num) => total + num, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const result = array.reduce((total, num) => total * num, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	const result = num1 ** num2;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
